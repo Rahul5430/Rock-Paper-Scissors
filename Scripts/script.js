f2d2eb9f0071ba86712d5a5289873bfdbe6b1070
Rahul5430 <rahul213himani@gmail.com>
2020-08-12 22:38:51 +0530

    Final Commit

--- Scripts/script.js
+++ Scripts/script.js
@@ -55,36 +55,36 @@ function computerPlay() {
 function playRound(playerSelection) {
     let computerSelection = computerPlay();
     rounds++;
-    rounds < 5 ? round.textContent = 'Round ${rounds + 1}/5' : round.textContent = 'Round ${rounds}/5';
+    rounds < 5 ? round.textContent = `Round ${rounds + 1}/5` : round.textContent = `Round ${rounds}/5`;
     if (playerSelection === computerSelection) {
-        scoreboard.textContent = '${playerscore} : ${compscore}';
+        scoreboard.textContent = `${playerscore} : ${compscore}`;
     }
     else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
         playerscore++;
-        scoreboard.textContent = '${playerscore} : ${compscore}'; 
+        scoreboard.textContent = `${playerscore} : ${compscore}`; 
     }
     else if ((playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock')) {
         compscore++;
-        scoreboard.textContent = '${playerscore} : ${compscore}'; 
+        scoreboard.textContent = `${playerscore} : ${compscore}`; 
     }
 
     if (playerscore > compscore) {
-        gameStatus.textContent = "I chose ${computerSelection}. You're ahead! For now...";
+        gameStatus.textContent = `I chose ${computerSelection}. You're ahead! For now...`;
     }
     else if (playerscore < compscore) {
-        gameStatus.textContent = "I chose ${computerSelection}. Better catch up!";
+        gameStatus.textContent = `I chose ${computerSelection}. Better catch up!`;
     }
     else {
-        gameStatus.textContent = "I chose ${computerSelection}. It seems I am only as worthy as you, human.";
+        gameStatus.textContent = `I chose ${computerSelection}. It seems I am only as worthy as you, human.`;
     }
 
-    if (round === 5) {
+    if (rounds === 5) {
         if (playerscore > compscore) result = 'You Win!';
         else if (playerscore < compscore) result = 'You Lose!';
         else result = "It's a Draw!";
         round.textContent = 'GAME OVER!';
         gameStatus.style.backgroundColor = '#ddd';
-        gameStatus.innerHTML = 'I chose ${computerSelection}. ${result} <br><strong style="calc(font-size: 1.3rem + 0.6vw)">Play Again?</strong>';
+        gameStatus.innerHTML = `I chose ${computerSelection}. ${result} <br><strong style="calc(font-size: 1.3rem + 0.6vw)">Play Again?</strong>`;
     }
     return gameStatus;
 }
@@ -95,9 +95,9 @@ function reset() {
     if (rounds === 5) {
         return playerscore = 0,
         compscore = 0,
-        rounds = 0;
-        scoreboard.textContent = '${playerscore} : ${compscore}',
-        round.textContent = 'Round ${rounds + 1}/5',
+        rounds = 0,
+        scoreboard.textContent = `${playerscore} : ${compscore}`,
+        round.textContent = `Round ${rounds + 1}/5`,
         gameStatus.textContent = 'Good Luck!',
         gameStatus.style.backgroundColor = '#fff',
         currentlyPlaying = true;
